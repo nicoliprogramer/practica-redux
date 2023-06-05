e7b33d73ed89cb6853b3b4896d78ae5de2edba18
nicoliprogramer <imotivacionesf@gmail.com>
2023-06-05 14:18:05 -0300

    add a product

--- app.js
+++ app.js
@@ -20,16 +20,19 @@ const return_pokemon_action = (cant) => {
 //###### Reducers
 const default_games_state = {
   pokemon: 10,
+  yoshi: 10,
 };
 const games_reducer = (state = default_games_state, action) => {
   switch (action.type) {
     case BUY_POKEMON: {
       return {
+        ...state,
         pokemon: state.pokemon - action.payload,
       };
     }
     case RETURN_POKEMON: {
       return {
+        ...state,
         pokemon: state.pokemon + action.payload,
       };
     }
