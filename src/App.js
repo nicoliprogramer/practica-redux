aa8b00836cd3a8c8e1a936ef7d0e301202ba1f50
nicoliprogramer <imotivacionesf@gmail.com>
2023-06-06 10:04:36 -0300

    creating the redux structure inside react

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import CantidadPokemon from "./components/CantidadPokemon";
+import CompraPokemon from "./components/CompraPokemon";
+import store from "./redux/store";
+
+function App() {
+  console.log(store.getState());
+
+  return (
+    <div className="App container">
+      <div className="row">
+        <div className="col-12">
+          <div className="card mt-5" style={{ maxWidth: "370px" }}>
+            <div className="row no-gutters">
+              <div className="col-4">
+                <img
+                  src={require("./img/pokemon.png")}
+                  alt="pokemon"
+                  className="card-img"
+                />
+              </div>
+              <div className="col-8">
+                <div className="card-body">
+                  <div className="card-title h3 text-center">
+                    <CantidadPokemon />
+                  </div>
+                  <CompraPokemon />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
