6676096d328e592db6ba6a8c15cdb880ee03023f
nicoliprogramer <imotivacionesf@gmail.com>
2023-06-06 10:12:13 -0300

    connecting react to redux

--- src/App.js
+++ src/App.js
@@ -7,35 +7,37 @@ import CantidadPokemon from "./components/CantidadPokemon";
 import CompraPokemon from "./components/CompraPokemon";
 import store from "./redux/store";
 
-function App() {
-  console.log(store.getState());
+import { Provider } from "react-redux";
 
+function App() {
   return (
-    <div className="App container">
-      <div className="row">
-        <div className="col-12">
-          <div className="card mt-5" style={{ maxWidth: "370px" }}>
-            <div className="row no-gutters">
-              <div className="col-4">
-                <img
-                  src={require("./img/pokemon.png")}
-                  alt="pokemon"
-                  className="card-img"
-                />
-              </div>
-              <div className="col-8">
-                <div className="card-body">
-                  <div className="card-title h3 text-center">
-                    <CantidadPokemon />
+    <Provider store={store}>
+      <div className="App container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card mt-5" style={{ maxWidth: "370px" }}>
+              <div className="row no-gutters">
+                <div className="col-4">
+                  <img
+                    src={require("./img/pokemon.png")}
+                    alt="pokemon"
+                    className="card-img"
+                  />
+                </div>
+                <div className="col-8">
+                  <div className="card-body">
+                    <div className="card-title h3 text-center">
+                      <CantidadPokemon />
+                    </div>
+                    <CompraPokemon />
                   </div>
-                  <CompraPokemon />
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
