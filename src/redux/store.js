ec902b88db803da630d271ebcdbea665b0ee1e4f
nicoliprogramer <imotivacionesf@gmail.com>
2023-06-06 10:21:59 -0300

    Using Logger Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import rootReducers from "./reducers/rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducers);
+const store = createStore(rootReducers, composeWithDevTools());
 
 export default store;
