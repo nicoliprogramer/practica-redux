37b3ebb9a9b0361af31a139f6b0ba433e31202b5
nicoliprogramer <imotivacionesf@gmail.com>
2023-06-06 13:07:47 -0300

    creation of reducers

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import game_shop from "./gameShopReducer";
+import buscador from "./buscadorReducer";
 
 const rootReducers = combineReducers({
   game_shop,
+  buscador,
 });
 
 export default rootReducers;
